a2b00fe84b7b467394135f3d6588d84588741d47
eolhing <eolhing@gmail.com>
2012-12-04 23:48:19 +0100

    Clean main

--- src/main.c
+++ src/main.c
@@ -18,46 +18,13 @@ int main(int argc, char* argv[])
 
 	programOptions po = programOptionsCreate();
 
-	while (readParam(argc, argv, po) == 0)
+	while (readArgument(argc, argv, po) == 0)
 	{
 	}
-
-	printf("%d\n", programOptionsGetMode(po));
 
 	executeCommand(po);
 
 	programOptionsDestroy(po);
-
-	/*
-	 char* dirName = (argc < 2) ? "." : argv[1];
-
-	 struct stat myStat;
-	 stat(dirName, &myStat);
-
-	 if(!S_ISDIR(myStat.st_mode))
-	 EXIT_FAILURE;
-
-	 DIR* dir = opendir(dirName);
-	 struct dirent** namelist;
-	 struct dirent* entry = readdir(dir);
-
-	 while(entry != NULL)
-	 {
-	 printf("%s\n", (char*) entry->d_name);
-	 entry = readdir(dir);
-	 }
-
-	 struct dirent** namelist;
-
-	 int fileCount = scandir(dirName, &namelist, NULL, alphasort);
-
-	 unsigned int i = 0;
-	 for(; i < fileCount; ++i)
-	 {
-	 struct dirent* file = namelist[i];
-	 printf("%s\n", (char*) file->d_name);
-	 }
-	 */
 
 	return EXIT_SUCCESS;
 }
